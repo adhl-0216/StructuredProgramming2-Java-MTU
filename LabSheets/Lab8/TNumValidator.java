cf11977b41e8b7d6fb0117b91e43c05e7d5971a3
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-11 16:57:11 +0100

    221011

--- LabSheets/Lab8/TNumValidator.java
+++ LabSheets/Lab8/TNumValidator.java
@@ -7,27 +7,35 @@ public class TNumValidator {
     public static void main(String[] args) {
         String[] allTNums = new String[3];
         StringBuilder output = new StringBuilder();
-        int i = 0;
-        String tNum;
-        String msg = "Please enter a T-Number (Return to Exit)";
-        boolean valid = false;
-        while(i < 3) {
-            tNum = JOptionPane.showInputDialog(msg);
-            if (!(tNum == null)){
-                if ((tNum.length()==9 && tNum.toLowerCase(Locale.ROOT).charAt(0)=='t')) {
-                    allTNums[i] = tNum;
-                    i++;
+        boolean valid;
+
+        for (int i = 0; i < allTNums.length ; i++) {
+            String tNum = JOptionPane.showInputDialog("Please enter a T-Number (Return to Exit)");
+            valid = false;
+            while (!valid) {
+                if ((tNum == null)|| tNum.equals("")){
+                    JOptionPane.showMessageDialog(null,"You entered no valid T-Numbers!");
+                    System.exit(0);
+                }
+                else {
+                    if (!(tNum.length()==9 && tNum.toLowerCase(Locale.ROOT).charAt(0)=='t')) {
+                        tNum = JOptionPane.showInputDialog("Invalid! Please enter a T-Number (Return to Exit)");
+                    }
+                    else {
+                        for (int c = 1; c < 9 ; c++) {
+                            if (!Character.isDigit(tNum.charAt(c))) {
+                                tNum = JOptionPane.showInputDialog("Invalid! Please enter a T-Number (Return to Exit)");
+                            }
+                        }
+                        valid = true;
+                        allTNums[i] = tNum;
+                    }
                 }
             }
-            else {
-                JOptionPane.showMessageDialog(null,"You entered no valid T-Numbers!");
-                System.exit(0);
-            }
         }
+
         output.append("Number of valid T-Numbers entered: 3\nList of valid T-Numbers:\n\n");
-        for (String str:allTNums) {
-            output.append(str).append("\n");
-        }
+        for (String str:allTNums) output.append(str).append("\n");
 
         JOptionPane.showMessageDialog(null, output,"Valid T-Numbers", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
